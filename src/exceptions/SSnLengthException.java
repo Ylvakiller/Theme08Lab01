77e882eef15bf54f2b5f10fc10edb043552e70e6
Ylvakiller <rem.geuze@gmail.com>
2015-05-01 00:19:18 +0200

    Wrote the getSSN method
    This method checks if the given input is correct, throw exceptions if it
    was not correct telling what was wrong and will return the int form of
    the SSN if it was of correct form

--- src/exceptions/SSnLengthException.java
+++ src/exceptions/SSnLengthException.java
@@ -7,24 +7,24 @@ package exceptions;
  * @author Ylva
  *
  */
-public class SSnLengthException extends Exception {
+public class SSNLengthException extends Exception {
 	/**
 	 * The reason for this number is given in the TimeFormatException class
 	 */
 	private static final long serialVersionUID = 1L;
-	public SSnLengthException() {
+	public SSNLengthException() {
 		super("The SSN is of incorrect length.");
 	}
-	public SSnLengthException(String arg0) {
+	public SSNLengthException(String arg0) {
 		super(arg0);
 	}
-	public SSnLengthException(Throwable arg0) {
+	public SSNLengthException(Throwable arg0) {
 		super(arg0);
 	}
-	public SSnLengthException(String arg0, Throwable arg1) {
+	public SSNLengthException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 	}
-	public SSnLengthException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+	public SSNLengthException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
 	}
 
--- src/program2/Employee.java
+++ src/program2/Employee.java
@@ -3,6 +3,9 @@
  */
 package program2;
 
+import exceptions.SSNCharacterException;
+import exceptions.SSNLengthException;
+
 /**
  * @author Ylva
  *
@@ -33,6 +36,7 @@ public class Employee extends Person {
 	/**
 	 * Sets only the SSN
 	 * Uses the default constructor of the super class to set the name
+	 * Here the SSN has to be checked beforehand using the Employee.checkSSN(String) method
 	 * @param initialSSN the SSN to set
 	 */
 	public Employee (int initialSSN){
@@ -57,10 +61,35 @@ public class Employee extends Person {
 	}
 
 	/**
-	 * @param SSN the sSN to set
+	 * @param initialSSN the SSN to set
 	 */
 	public void setSSN(int initialSSN) {
 		SSN = initialSSN;
+	}
+	
+	/**
+	 * Will return the integer form of the SSN if the SSN is a possible SSN
+	 * @param SSN The SSN input to check
+	 * @return The integer version of the SSN
+	 * @throws SSNLengthException This exception is thrown if the SSN not including spaces or dashes is not of length 9
+	 * @throws SSNCharacterException This exception is thrown if the SSN not including spaces or dashes contains non digit characters
+	 */
+	public static int getSSN(String SSN) throws SSNLengthException, SSNCharacterException{
+		SSN = SSN.replace(" ", "");//take out the spaces
+		SSN = SSN.replace("-", "");//take out the dashes
+		if (SSN.length()>9){
+			throw new SSNLengthException("The given SSN is to long.\nYou gave the SSN : " + SSN + ".\nThe SSN should only be 9 characters and this SSN is " + SSN.length() + " characters.");
+		}else if (SSN.length()<9){
+			throw new SSNLengthException("The given SSN is to short.\nYou gave the SSN : " + SSN + ".\nThe SSN should be 9 characters and this SSN is " + SSN.length() + " characters.");
+		}
+	    int i = 0;
+	    while (i<9){
+	    	if (!Character.isDigit(SSN.charAt(i))){
+	    		throw new SSNCharacterException("The given SSN contains a non digit character.\nYou gave the SSN : " + SSN + ".\nThe caracter at position " + (i+1) + " is a " + SSN.charAt(i) + ".\nThis is not a digit");
+	    	}
+	    	i++;
+	    }
+		return Integer.parseInt(SSN);
 	}
 	
 
